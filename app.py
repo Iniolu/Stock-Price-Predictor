547a074d1ac2d16c70e98043f22c7e6578d2de26
Daniel Akanmu <akanmu@ualberta.ca>
2024-03-14 13:27:39 -0600

    torch installed

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request, jsonify
 import yfinance as yf
 import pandas as pd
-# import torch
-# import torch.nn as nn
+import torch
+import torch.nn as nn
 import numpy as np
 # from sklearn.preprocessing import MinMaxScaler
 
